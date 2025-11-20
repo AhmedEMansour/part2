d3eb9418d07a41ce6a0027a331a731adcb8c11a7
AhmedEMansour <ahmedezzatmansour1@gmail.com>
2025-11-20 16:36:50 +0300

    2.8

--- the-phonebook/src/App.jsx
+++ the-phonebook/src/App.jsx
@@ -2,23 +2,40 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   
-  const handlePersonChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value)
-    console.log(newName);
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   const addPerson = (e) => {
     e.preventDefault()
     const personObj = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
+    persons.some(person => person.name === newName) 
+    ?
+    window.alert(`${newName} is already added to phonebook`) 
+    : 
+    persons.some(person => person.number === newNumber)
+    ?
+    window.alert(`a person already exists with the number ${newNumber}`)
+    :
     setPersons([...persons, personObj])
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -26,7 +43,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange} />
+          name: 
+          <input 
+            name='name'
+            autoComplete='name'
+            value={newName}
+            onChange={handleNameChange}
+          /> <br />
+          number:
+          <input 
+            name='number'
+            autoComplete='number'
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +65,7 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map(person => (
         <p key={person.name}>
-          {person.name}
+          {person.name} {person.number}
         </p>
       ))}
       
