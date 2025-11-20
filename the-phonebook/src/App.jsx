82bf1a0e5534c97e189391234f790534b0f606f1
AhmedEMansour <ahmedezzatmansour1@gmail.com>
2025-11-20 15:48:11 +0300

    2.6

--- the-phonebook/src/App.jsx
+++ the-phonebook/src/App.jsx
@@ -5,20 +5,39 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  
+  
+  const handlePersonChange = (e) => {
+    setNewName(e.target.value)
+    console.log(newName);
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObj = {
+      name: newName
+    }
+    setPersons([...persons, personObj])
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => (
+        <p key={person.name}>
+          {person.name}
+        </p>
+      ))}
       
     </div>
   )
